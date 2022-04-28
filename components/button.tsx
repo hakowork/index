8f6b4d5d0188fceeaa74e329d81c46640978731a
datoyacx <datoyacx@outlook.jp>
2022-04-28 20:53:13 +0700

    add noopener noreferrer

--- components/button.tsx
+++ components/button.tsx
@@ -1,6 +1,6 @@
 const Button = ({ title, url }: { title?: string, url?: string }) => {
     return (
-        <a target="_blank" href={url} className="inline-block font-mont uppercase mt-8 mb-6 sm:mb-0 text-lg border border-gray-50 py-2 px-6 bg-transparent text-gray-50 hover:bg-gray-50 hover:text-gray-900 duration-200">
+        <a target="_blank" href={url} rel="noopener noreferrer" className="inline-block font-mont uppercase mt-8 mb-6 sm:mb-0 text-lg border border-gray-50 py-2 px-6 bg-transparent text-gray-50 hover:bg-gray-50 hover:text-gray-900 duration-200">
             <span className="text-md sm:text-lg duration-200">{title}</span>
         </a>
     )
